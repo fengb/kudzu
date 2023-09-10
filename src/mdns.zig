5314247455128990f7eb2ce851d9b2ac41e68b07
Benjamin Feng <contact@fengb.me>
2023-09-09 22:33:17 -0500

    Preliminary question parsing

--- src/mdns.zig
+++ src/mdns.zig
@@ -27,19 +27,65 @@ pub const DnsMessage = struct {
     additional_record_count: u16,
 
     payload: []const u8,
+    raw_questions: []const u8,
 
-    pub fn parse(data: []const u8) DnsMessage {
+    pub fn parse(data: []const u8) !DnsMessage {
+        const question_count = std.mem.readIntBig(u16, data[4..6]);
+        const answer_record_count = std.mem.readIntBig(u16, data[6..8]);
+        const authority_record_count = std.mem.readIntBig(u16, data[8..10]);
+        const additional_record_count = std.mem.readIntBig(u16, data[10..12]);
+
+        const payload = data[12..];
+        var fbs = std.io.fixedBufferStream(payload);
+        const start = fbs.pos;
+        for (0..1) |_| {
+            _ = try QuestionIterator.nextRaw(&fbs);
+        }
+        const raw_questions = payload[start..fbs.pos];
+
         return .{
             .identification = std.mem.readIntBig(u16, data[0..2]),
             .flags = @bitCast(data[2..4].*),
-            .question_count = std.mem.readIntBig(u16, data[4..6]),
-            .answer_record_count = std.mem.readIntBig(u16, data[6..8]),
-            .authority_record_count = std.mem.readIntBig(u16, data[8..10]),
-            .additional_record_count = std.mem.readIntBig(u16, data[10..12]),
+            .question_count = question_count,
+            .answer_record_count = answer_record_count,
+            .authority_record_count = authority_record_count,
+            .additional_record_count = additional_record_count,
 
-            .payload = data[12..],
+            .payload = payload,
+            .raw_questions = raw_questions,
         };
     }
+
+    const Question = struct {
+        name: EncodedString,
+        type: u16,
+        class: u16,
+    };
+
+    pub fn iterQuestions(self: DnsMessage) QuestionIterator {
+        return QuestionIterator{ .fbs = std.io.fixedBufferStream(self.raw_questions) };
+    }
+
+    const QuestionIterator = struct {
+        fbs: std.io.FixedBufferStream([]const u8),
+
+        pub fn next(self: *QuestionIterator) ?Question {
+            return nextRaw(&self.fbs) catch unreachable;
+        }
+
+        fn nextRaw(fbs: *std.io.FixedBufferStream([]const u8)) !?Question {
+            if (fbs.pos >= fbs.buffer.len) {
+                return null;
+            }
+            const reader = fbs.reader();
+
+            return Question{
+                .name = try EncodedString.readFirst(fbs),
+                .type = try reader.readIntBig(u16),
+                .class = try reader.readIntBig(u16),
+            };
+        }
+    };
 };
 
 pub const EncodedString = struct {
@@ -105,4 +151,30 @@ test EncodedString {
 
     const es = try EncodedString.readFirst(&fbs);
     try std.testing.expectEqualStrings("www.xyzindustries.com", try std.fmt.bufPrint(&buffer, "{}", .{es}));
+}
+
+test DnsMessage {
+    {
+        const questions1 = @embedFile("test-data/questions1.dns");
+        const message = try DnsMessage.parse(questions1);
+        var iter = message.iterQuestions();
+        while (iter.next()) |question| {
+            std.debug.print("{any}\n", .{question});
+        }
+        std.debug.print("{any}\n", .{message.raw_questions});
+        std.debug.print("{any}\n", .{message.payload});
+    }
+
+    std.debug.print("\n\n", .{});
+
+    {
+        const questions2 = @embedFile("test-data/questions2.dns");
+        const message = try DnsMessage.parse(questions2);
+        var iter = message.iterQuestions();
+        while (iter.next()) |question| {
+            std.debug.print("{any}\n", .{question});
+        }
+        std.debug.print("{any}\n", .{message.raw_questions});
+        std.debug.print("{any}\n", .{message.payload});
+    }
 }
